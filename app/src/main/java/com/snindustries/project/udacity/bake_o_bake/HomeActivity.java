08a986dae163235d80602aff261b75e209a6a430
shaazps <shaaz@perfectsensedigital.com>
2018-11-09 13:27:43 -0500

    Blank Project

---
+++ app/src/main/java/com/snindustries/project/udacity/bake_o_bake/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.snindustries.project.udacity.bake_o_bake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
