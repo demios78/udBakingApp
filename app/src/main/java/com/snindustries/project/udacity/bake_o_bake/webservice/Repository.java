76d113812c63f08044429a2663b042fa15b015dd
shaazps <shaaz@perfectsensedigital.com>
2018-11-12 17:17:47 -0500

    Cleanup

--- app/src/main/java/com/snindustries/project/udacity/bake_o_bake/webservice/Repository.java
+++ app/src/main/java/com/snindustries/project/udacity/bake_o_bake/webservice/Repository.java
@@ -1,10 +1,10 @@
 package com.snindustries.project.udacity.bake_o_bake.webservice;
 
-import android.arch.core.util.Function;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.Transformations;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.orhanobut.logger.Logger;
 import com.snindustries.project.udacity.bake_o_bake.webservice.model.Recipe;
@@ -29,43 +29,15 @@ public class Repository {
     private final MutableLiveData<Integer> stepQuery = new MutableLiveData<>();
     private LiveData<Step> currentStep = Transformations.map(stepQuery, this::getStep);
     private MutableLiveData<List<Recipe>> recipes = new MutableLiveData<>();
-    private LiveData<Recipe> currentRecipe = Transformations.switchMap(recipeQuery, new Function<Integer, LiveData<Recipe>>() {
-        @Override
-        public LiveData<Recipe> apply(Integer input) {
-
-            MutableLiveData<Recipe> out = new MutableLiveData<>();
-            out.postValue(getRecipe(input));
-
-            return out;
-        }
+    private LiveData<Recipe> currentRecipe = Transformations.switchMap(recipeQuery, input -> {
+        MutableLiveData<Recipe> out = new MutableLiveData<>();
+        out.postValue(getRecipe(input));
+        return out;
     });
-    //private LiveData<Recipe> currentRecipe = Transformations.map(recipeQuery, this::getRecipe);
 
     private Repository() {
-        RecipeClient.get().getApi().getRecipies().enqueue(
-                new Callback<List<Recipe>>() {
-                    @Override
-                    public void onFailure(@NonNull Call<List<Recipe>> call, @NonNull Throwable t) {
-                        t.printStackTrace();
-                    }
-
-                    @Override
-                    public void onResponse(@NonNull Call<List<Recipe>> call, @NonNull Response<List<Recipe>> response) {
-                        if (response.body() != null) {
-                            recipes.postValue(response.body());
-                            Logger.d(response.body());
-
-                        } else {
-                            Logger.e("No Response");
-                        }
-                    }
-                }
-        );
-        currentRecipe.observeForever(recipe -> {
-            if (recipe != null && recipe.steps != null && recipe.steps.size() > 0 && recipe.steps.get(0).id != null) {
-                applyCurrentStep(recipe.steps.get(0).id);
-            }
-        });
+        RecipeClient.get().getApi().getRecipies().enqueue(new GetRecipeList());
+        currentRecipe.observeForever(this::resetStepOnRecipeChange);
     }
 
     public static Repository get() {
@@ -80,9 +52,7 @@ public class Repository {
     }
 
     public void applyCurrentRecipe(Integer index) {
-
         recipeQuery.postValue(index);
-
     }
 
     public void applyCurrentStep(Integer id) {
@@ -98,7 +68,10 @@ public class Repository {
     }
 
     private Recipe getRecipe(Integer input) {
-        return Objects.requireNonNull(recipes.getValue()).stream().filter(recipe -> recipe.id.equals(input)).collect(Collectors.toList()).get(0);
+        return Objects.requireNonNull(recipes.getValue())
+                .stream().filter(recipe -> recipe.id.equals(input))
+                .collect(Collectors.toList())
+                .get(0);
     }
 
     public LiveData<List<Recipe>> getRecipes() {
@@ -106,6 +79,34 @@ public class Repository {
     }
 
     private Step getStep(Integer input) {
-        return Objects.requireNonNull(getCurrentRecipe().getValue()).steps.stream().filter(step -> step.id.equals(input)).collect(Collectors.toList()).get(0);
+        return Objects.requireNonNull(getCurrentRecipe().getValue())
+                .steps.stream()
+                .filter(step -> step.id.equals(input))
+                .collect(Collectors.toList())
+                .get(0);
+    }
+
+    private void resetStepOnRecipeChange(@Nullable Recipe recipe) {
+        if (recipe != null && recipe.steps != null && recipe.steps.size() > 0 && recipe.steps.get(0).id != null) {
+            applyCurrentStep(recipe.steps.get(0).id);
+        }
+    }
+
+    private class GetRecipeList implements Callback<List<Recipe>> {
+        @Override
+        public void onFailure(@NonNull Call<List<Recipe>> call, @NonNull Throwable t) {
+            t.printStackTrace();
+        }
+
+        @Override
+        public void onResponse(@NonNull Call<List<Recipe>> call, @NonNull Response<List<Recipe>> response) {
+            if (response.body() != null) {
+                recipes.postValue(response.body());
+                Logger.d(response.body());
+
+            } else {
+                Logger.e("No Response");
+            }
+        }
     }
 }
